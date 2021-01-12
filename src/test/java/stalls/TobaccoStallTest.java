3bcbc06347e847017acb837cfb6110bc31f8ce8c
RatBoyJim <ratboyjim@gmail.com>
2021-01-12 19:47:15 +0000

    MVP done

--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -14,7 +14,7 @@ public class TobaccoStallTest {
 
     @Before
     public void setUp() throws Exception {
-        tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1, 8);
         visitor = new Visitor(44, 160, 30.79);
         visitorTooYoung = new Visitor(14, 150, 15.43);
     }
@@ -42,6 +42,11 @@ public class TobaccoStallTest {
     @Test
     public void tooYoungToBuyFags() {
         assertEquals(false, tobaccoStall.isAllowedTo(visitorTooYoung));
+    }
+
+    @Test
+    public void hasRating() {
+        assertEquals(8, tobaccoStall.getRating());
     }
 
 }
